a556e58ac46c10950ed973d293c67c209bf63819
Teo Froissart <tfroissa@gmail.com>
2024-07-18 17:40:13 +0200

    continue script p1 and vagrantfile

---
+++ p1/scripts/prep.sh
@@ -0,0 +1,7 @@
+echo "SERVER__IP:   <$SERVER__IP>"
+echo "SERVER__NAME: <$SERVER__NAME>"
+echo "AGENT__IP:    <$AGENT__IP>"
+echo "AGENT__NAME:  <$AGENT__NAME>"
+
+apt-get update && apt-get upgrade -y
+apt-get install -y curl net-tools
